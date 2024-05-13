6f7ff42279dd8e72f717481fa7640eaf706286d7
Ismayil <ismayilteymurov.444@gmail.com>
2024-05-13 17:41:33 +0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='container'>
+        <div className="image"></div>
+        <div className="processing">
+         
+            <h1>Consolidate Payroll Processing</h1>
+         
+          
+            <p>We have designed a fast and effective payroll system that streamlines your payment process.</p>
+         
+          <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default App
